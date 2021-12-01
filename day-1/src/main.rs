91205419917ab79c5a9cfa57545ae50f1a9a6633
David Träff <traff.david@gmail.com>
2021-12-01 08:46:31 +0100

    Day 1

---
+++ day-1/src/main.rs
@@ -0,0 +1,43 @@
+fn main() {
+    let input = include_str!("../input");
+    let depths: Vec<i32> = input
+        .split('\n')
+        .map(|l| l.trim().parse().expect("Invalid number"))
+        .collect();
+
+    println!(
+        "PART 1: Number of increases: {}",
+        windowed_depth_increases(&depths, 1)
+    );
+
+    println!(
+        "PART 2: Number of windowed increases {}",
+        windowed_depth_increases(&depths, 3)
+    );
+}
+
+fn windowed_depth_increases(depths: &Vec<i32>, size: usize) -> usize {
+    let windowed: Vec<i32> = depths
+        .windows(size)
+        .into_iter()
+        .map(|x| x.iter().sum())
+        .collect();
+
+    depth_increases(&windowed)
+}
+
+fn depth_increases(depths: &Vec<i32>) -> usize {
+    let mut increases = 0usize;
+    let mut previous = None;
+    for value in depths {
+        if let Some(previous) = previous {
+            if value > previous {
+                increases = increases + 1;
+            }
+        }
+
+        previous = Some(value);
+    }
+
+    increases
+}
